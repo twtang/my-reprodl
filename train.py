eaae41429fcd815fffce331b7f7c972c5408d0fb
twtang <tszw.tang@gmail.com>
2021-07-04 18:04:09 +0800

    extra_dvc_experiments_management

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import torch, torchaudio
 from torch import nn
 from torch.nn import functional as F
@@ -12,14 +11,9 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import hydra
-from omegaconf import DictConfig, OmegaConf
-
-import logging
-
-logger = logging.getLogger(__name__)
-
-import wandb
+import pdb
+import yaml
+import dvclive
 
 
 class ESC50Dataset(torch.utils.data.Dataset):
@@ -53,28 +47,20 @@ class ESC50Dataset(torch.utils.data.Dataset):
 
 
 class AudioNet(pl.LightningModule):
-    def __init__(self, hparams: DictConfig):
+    def __init__(self, n_classes=50, base_filters=32):
         super().__init__()
-
-        # Fundamentally, this will save the hyper-parameters
-        # in a way that is meaningful to PyTorch Lightning.
-        self.save_hyperparameters(hparams)
-
-        self.conv1 = nn.Conv2d(1, hparams.base_filters, 11, padding=5)
-        self.bn1 = nn.BatchNorm2d(hparams.base_filters)
-        self.conv2 = nn.Conv2d(hparams.base_filters, hparams.base_filters, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(hparams.base_filters)
+        self.conv1 = nn.Conv2d(1, base_filters, 11, padding=5)
+        self.bn1 = nn.BatchNorm2d(base_filters)
+        self.conv2 = nn.Conv2d(base_filters, base_filters, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(base_filters)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(
-            hparams.base_filters, hparams.base_filters * 2, 3, padding=1
-        )
-        self.bn3 = nn.BatchNorm2d(hparams.base_filters * 2)
-        self.conv4 = nn.Conv2d(
-            hparams.base_filters * 2, hparams.base_filters * 4, 3, padding=1
-        )
-        self.bn4 = nn.BatchNorm2d(hparams.base_filters * 4)
+        self.conv3 = nn.Conv2d(base_filters, base_filters * 2, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(base_filters * 2)
+        self.conv4 = nn.Conv2d(base_filters * 2, base_filters * 4, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(base_filters * 4)
         self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(hparams.base_filters * 4, hparams.n_classes)
+        self.fc1 = nn.Linear(base_filters * 4, n_classes)
+        self.last_acc = None
 
     def forward(self, x):
         x = self.conv1(x)
@@ -97,6 +83,11 @@ class AudioNet(pl.LightningModule):
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
         self.log("train_loss", loss, on_step=True)
+        # logging metric
+        train_loss = loss.data.cpu().numpy().reshape(1)[0].item()
+        dvclive.log("train_loss", train_loss)  # log metric
+        dvclive.next_step()
+
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -108,58 +99,48 @@ class AudioNet(pl.LightningModule):
         return acc
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.optimizer.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
 
-@hydra.main(config_path="configs", config_name="default")
-def train(cfg: DictConfig):
-
-    # Initialize the W&B agent using the default values from cfg
-    config = {
-        "sample_rate": cfg.data.sample_rate,
-        "lr": cfg.model.optimizer.lr,
-        "base_filters": cfg.model.base_filters,
-    }
-    wandb.init(project="reprodl", config=config)
-
-    # Get the (possibly updated) values from wandb
-    cfg.data.sample_rate = wandb.config.sample_rate
-    cfg.model.optimizer.lr = wandb.config.lr
-    cfg.model.base_filters = wandb.config.base_filters
-
-    # Simple logging of the configuration
-    logger.info(OmegaConf.to_yaml(cfg))
-
-    # We recover the original path of the dataset:
-    path = Path(hydra.utils.get_original_cwd() / Path(cfg.data.path))
+def train(params):
+    batch_size = params["batch_size"]
+    base_filters = params["base_filters"]
+    n_classes = params["n_classes"]
+    gpus = params["gpus"]
+    max_epochs = params["max_epochs"]
+    n_classes = params["n_classes"]
+    seed = params["seed"]
+    # This is the main training function requested by the exercise.
+    # We use folds 1,2,3 for training, 4 for validation, 5 for testing.
 
     # Load data
-    train_data = ESC50Dataset(
-        path=path, sample_rate=cfg.data.sample_rate, folds=cfg.data.train_folds
-    )
-    val_data = ESC50Dataset(
-        path=path, sample_rate=cfg.data.sample_rate, folds=cfg.data.val_folds
-    )
-    test_data = ESC50Dataset(
-        path=path, sample_rate=cfg.data.sample_rate, folds=cfg.data.test_folds
-    )
+    train_data = ESC50Dataset(folds=[1, 2, 3])
+    val_data = ESC50Dataset(folds=[4])
+    test_data = ESC50Dataset(folds=[5])
 
     # Wrap data with appropriate data loaders
     train_loader = torch.utils.data.DataLoader(
-        train_data, batch_size=cfg.data.batch_size, shuffle=True
+        train_data, batch_size=batch_size, shuffle=True
     )
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=cfg.data.batch_size)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=cfg.data.batch_size)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
 
-    pl.seed_everything(cfg.seed)
+    pl.seed_everything(seed)
 
     # Initialize the network
-    audionet = AudioNet(cfg.model)
-    wandb_logger = pl.loggers.WandbLogger()
-    trainer = pl.Trainer(**cfg.trainer, logger=wandb_logger)
+    audionet = AudioNet(n_classes, base_filters)
+    trainer = pl.Trainer(gpus=gpus, max_epochs=max_epochs)
     trainer.fit(audionet, train_loader, val_loader)
+
+    # summary dvc
+    accuracy = trainer.logged_metrics["train_loss"].data.cpu().numpy().reshape(1)[0]
+
+    summary_data = {"stages": {"train": {"accuracy": accuracy}}}
+    with open("summary.json", "w") as curr_file:
+        curr_file.write(str(summary_data))
 
 
 if __name__ == "__main__":
-    train()
+    params = yaml.safe_load(open("params.yaml"))["train"]
+    train(params)
